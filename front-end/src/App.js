31fc3df78dd3e9aef37988fbc2d7139e2e0e4f6a
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-06-22 12:01:06 +0300

    login commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import './App.css'
-import {BrowserRouter as Router, Route,Switch} from "react-router-dom";
-import {Home} from './components/home/Home';
-import { Header } from './components/header/Header';
-import { Login } from './components/login/Login';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./components/home/Home";
+import { Header } from "./components/header/Header";
+import { Login } from "./components/login/Login";
+import { auth } from "./firebase";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    //  will only run once when the app component loads...
+    auth.onAuthStateChange((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the suer just logged in/the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path={'/login'}>
-            <Login/>
+          <Route path={"/login"}>
+            <Login />
           </Route>
-          <Route path={'/'} exact>
-            <Header/>
-            <Home/>
-            
+          <Route path={"/"} exact>
+            <Header />
+            <Home />
           </Route>
         </Switch>
       </Router>
--- front-end/src/components/login/Login.jsx
+++ front-end/src/components/login/Login.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase";
 import "./Login.css";
 
-
 export const Login = () => {
-  const [email, setEmail] = useState('')
+  const history = useHistory();
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email.at, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        //   it successfully created a new user
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className="login">
@@ -32,19 +59,19 @@ export const Login = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="login__signInButton">Sign In</button>
+          <button type="submit" className="login__signInButton"  onClick={signIn}>Sign In</button>
         </form>
 
         <p>
-            {/* to be edited */}
+          {/* to be edited */}
           By signing-in you agree to the SENSEI ECOMMERCE Conditions of Use &
           Sale. Please see our Privacy Notice, our Cookies Notice and our
           Interest-Based Ads Notice.
         </p>
 
-        <button className="login__registerButton">
-            {/* to be edited */}
-            Create you Sensei Ecommerce Account
+        <button onClick={register} className="login__registerButton">
+          {/* to be edited */}
+          Create you Sensei Ecommerce Account
         </button>
       </div>
     </div>
