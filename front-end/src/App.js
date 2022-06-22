60cbef3028af9a0248af8f270b05248a3353dd86
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-06-22 14:31:15 +0300

    login commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,10 +6,29 @@ import { Header } from "./components/header/Header";
 import { Login } from "./components/login/Login";
 import { auth } from "./firebase";
 import { useEffect } from "react";
+import { useStateValue } from "./StateProvider";
+import { isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
 
 function App() {
+  console.log(auth);
+  const [{ user }, dispatch] = useStateValue();
   useEffect(() => {
     //  will only run once when the app component loads...
+
+    // if (isSignInWithEmailLink(auth, window.location.href)) {
+    //   let email = window.localStorage.getItem("emailForSignIn");
+    //   if (!email) {
+    //     email = window.prompt("Please provide your email for confirmation");
+    //   }
+    //   signInWithEmailLink(auth, email, window.location.href)
+    //     .then((result) => {
+    //       window.localStorage.removeItem("emailForSignIn");
+    //     })
+    //     .catch((error) => {
+    //       console.error(error.message);
+    //     });
+    // }
+
     auth.onAuthStateChange((authUser) => {
       console.log("THE USER IS >>> ", authUser);
 
--- front-end/src/firebase.js
+++ front-end/src/firebase.js
@@ -3,6 +3,7 @@ import firebase from "firebase/compat/app";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {getAuth} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,6 +24,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const db = app.firestore;
-const auth = firebase.auth;
+const auth = getAuth()
 
 export { db, auth };
