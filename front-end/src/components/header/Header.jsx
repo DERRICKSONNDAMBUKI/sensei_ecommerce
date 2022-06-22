4e6374de8425e1c64c7602a5acb00037279fdc97
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-06-22 09:54:48 +0300

    login commit

--- front-end/src/components/header/Header.jsx
+++ front-end/src/components/header/Header.jsx
@@ -25,10 +25,12 @@ export const Header = () => {
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
       </div>
+
+      {/* login */}
       <div className="header__nav">
         <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineTwo">Sign In</span>
+          <span className="header__optionLineOne">Hello {user?'Guest':user.email}</span>
+          <span className="header__optionLineTwo">{user?'Sign Out':'Sign In'}</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
